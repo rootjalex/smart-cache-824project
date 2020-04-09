4f052939307c32c6f97eccd7a69c1ec5af050272
Alexander Root <aroot222@mit.edu>
2020-04-08 20:57:25 -0400

    better testing print statements

--- src/cache_test.go
+++ src/cache_test.go
@@ -2,17 +2,28 @@ package cache
 
 import (
 	"testing"
+	"fmt"
 )
 
 func TestHello(t *testing.T) {
+	fmt.Printf("TestHelloBadCache ...\n")
+	failed := false
 	want := "input/hello.txt"
 	var cache BadCache
 	_, err := cache.Fetch(want)
 	if err != nil {
 		t.Errorf("Could not open %s", want)
+		failed = true
 	}
 	hits, misses := cache.Report()
 	if hits != 0 || misses != 1 {
 		t.Errorf("Expected 0 hits and 1 miss, got %d hits and %d misses.", hits, misses)
+		failed = true
+	}
+
+	if failed {
+		fmt.Printf("\t... FAILED\n")
+	} else {
+		fmt.Printf("\t... PASSED\n")
 	}
 }
--- src/heap_test.go
+++ src/heap_test.go
@@ -2,12 +2,16 @@ package cache
 
 import (
 	"testing"
+	"fmt"
 )
 
 func TestBasicHeap(t *testing.T) {
+	fmt.Printf("TestBasicHeap ...\n")
+	failed := false
+
 	var heap MinHeap
 	heap.Init()
-	
+
 	heap.Insert("first", -1)
 	heap.Insert("fourth", 5)
 	heap.Insert("second", 2)
@@ -17,29 +21,42 @@ func TestBasicHeap(t *testing.T) {
 	label := heap.ExtractMin()
 	if label != "first" {
 		t.Errorf("Expected 'first', got %s", label)
+		failed = true
 	}
 	label = heap.ExtractMin()
 	if label != "second" {
 		t.Errorf("Expected 'second', got %s", label)
+		failed = true
 	}
 	label = heap.ExtractMin()
 	if label != "third" {
 		t.Errorf("Expected 'third', got %s", label)
+		failed = true
 	}
 	heap.Insert("fifth", 100)
 	label = heap.ExtractMin()
 	if label != "fourth" {
 		t.Errorf("Expected 'fourth', got %s", label)
+		failed = true
 	}
 	label = heap.ExtractMin()
 	if label != "fifth" {
 		t.Errorf("Expected 'fifth', got %s", label)
+		failed = true
 	}
 	label = heap.ExtractMin()
 	if label != "last" {
 		t.Errorf("Expected 'last', got %s", label)
+		failed = true
 	}
 	if heap.n != 0 {
 		t.Errorf("Expected 0 items left, got %d items", heap.n)
+		failed = true
+	}
+
+	if failed {
+		fmt.Printf("\t... FAILED\n")
+	} else {
+		fmt.Printf("\t... PASSED\n")
 	}
 }
