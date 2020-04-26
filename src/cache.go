5c6a50cc57680f1ca94ac48a8a1c098222601a9e
Alexander Root <aroot222@mit.edu>
2020-04-26 18:46:44 -0400

    minor interface reworking

--- src/cache.go
+++ src/cache.go
@@ -4,6 +4,16 @@ import (
 	"os"
 )
 
+type PrefetchInterface interface {
+	Prefetch(filename string, n int) []string
+	Access(filename string)
+}
+
+type EvictionInterface interface {
+	Evict(filename string) string
+	Access(filename string)
+}
+
 /********************************
 Cache supports the following external API to users
 
@@ -16,7 +26,26 @@ c.Fetch(name string) (*os.File, error)
 
 *********************************/
 type Cache interface {
+	PrefetchInterface
+	EvictionInterface
 	Init(cacheSize int)
 	Fetch(name string) (*os.File, error)
 	Report() (int, int) // Cache hits, cache misses
 }
+
+
+type Thing struct {
+	w int 
+}
+
+func (t *Thing) What() int {
+	return t.w
+}
+
+type OtherThing struct {
+	Thing
+}
+
+// var t OtherThing 
+// t.w = 4
+// fmt.Println(t.What()) -> 4
