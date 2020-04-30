ff443cb2e44bf7c7defbc996277b47511744af34
Alexander Root <aroot222@mit.edu>
2020-04-30 13:30:00 -0400

    update cache to use datastore.DataType and edit tests accordingnly. rm bad_cache example

--- src/cache/cache.go
+++ src/cache/cache.go
@@ -1,43 +1,47 @@
 package cache
 
 import (
-	"os"
 	"sync"
 	"../heap"
 	"../markov"
+	"../datastore"
+	"errors"
 )
 
 /********************************
 Cache supports the following external API to users
 
-c.Init(cacheSize int, cacheType CacheType)
-    Initializes a cache with eviction policy and prefetch defined by cache type
+c.Init(cacheSize int, cacheType CacheType, data *datastore.DataStore)
+	Initializes a cache with eviction policy and prefetch defined by cache type
+	Copies underlying datastore
 c.Report() (hits, misses)
     Get a report of the hits and misses  TODO: Do we want a version number or
     timestamp mechanism of any form here?
-c.Fetch(name string) (*os.File, error)
+c.Fetch(name string) (datastore.DataType, error)
 
 *********************************/
 type Cache struct {
 	mu          sync.Mutex          // Lock to protect shared access to cache
 	misses		int
 	hits		int
-	cache		map[string]*os.File
+	cache		map[string]datastore.DataType
 	heap		heap.MinHeap
 	timestamp	int64 // for controlling LRU heap
 	cacheSize	int
 	chain 		*markov.MarkovChain
 	cacheType	CacheType
+	data 		*datastore.DataStore
 }
 
-
-func (c *Cache) Init(cacheSize int, cacheType CacheType) {
+// copies underlying datastore
+func (c *Cache) Init(cacheSize int, cacheType CacheType, data *datastore.DataStore) {
 	c.cacheType = cacheType
 	c.misses = 0
 	c.hits = 0
     c.cacheSize = cacheSize
-	c.cache = make(map[string]*os.File)
+	c.cache = make(map[string]datastore.DataType)
 	c.timestamp = 0
+	c.data = data.Copy()
 	
 	if cacheType == LRU || cacheType == MarkovEviction {
 		// only LRU caches should use heap
@@ -49,7 +53,7 @@ func (c *Cache) Init(cacheSize int, cacheType CacheType) {
 	}
 }
 
-func (c *Cache) Fetch(name string) (*os.File, error) {
+func (c *Cache) Fetch(name string) (datastore.DataType, error) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
@@ -72,6 +76,11 @@ func (c *Cache) Fetch(name string) (*os.File, error) {
 	} else {
 		c.AddToCache(name)
 		c.misses++
+		file, ok = c.cache[name]
+		if !ok {
+			// failed again - should not happen
+			err = errors.New("failed")
+		}
 	}
 	c.timestamp++
 
@@ -88,7 +97,7 @@ func (c *Cache) Report() (int, int) {
 
 // TODO: REPLACEMENT POLICY FOR MARKOV CHAIN
 // assumes mu is Locked
-func (c *Cache) replace(name string, file *os.File) {
+func (c *Cache) replace(name string, file datastore.DataType) {
 	c.cache[name] = file
 	c.heap.Insert(name, c.timestamp)
 	if c.heap.Size > c.cacheSize {
@@ -114,8 +123,9 @@ func (c *Cache) AddToCache(filename string) bool {
 	_, ok := c.cache[filename]
 
 	if !ok {
-		file, _ := os.Open(filename)
+		file, err := c.data.Get(filename)
 		c.replace(filename, file) // handles insertion into heap
+		return err
 	}
 	return ok
 }
--- src/cache/cache_test.go
+++ src/cache/cache_test.go
@@ -4,58 +4,35 @@ import (
 	"testing"
 	"fmt"
 	"strconv"
-	"os"
+	"../datastore"
 )
 
-func TestHelloBadCache(t *testing.T) {
-	fmt.Printf("TestHelloBadCache ...\n")
-	failed := false
-	want := "../input/hello.txt"
-	var cache BadCache
-	_, err := cache.Fetch(want)
-	if err != nil {
-		t.Errorf("Could not open %s", want)
-		failed = true
-	}
-	hits, misses := cache.Report()
-	if hits != 0 || misses != 1 {
-		t.Errorf("Expected 0 hits and 1 miss, got %d hits and %d misses.", hits, misses)
-		failed = true
-	}
-
-	if failed {
-		fmt.Printf("\t... FAILED\n")
-	} else {
-		fmt.Printf("\t... PASSED\n")
-	}
-}
-
-// assumes CACHE_SIZE <= 100
 func TestBasicLRUFail(t *testing.T) {
 	fmt.Printf("TestBasicLRUFail ...\n")
 	failed := false
 	misses := 0
 
+	data := datastore.MakeDataStore()
+
+	// add files to datastore
+	for j := 0; j < (CACHE_SIZE + 1); j++ {
+		filename := "fake_" + strconv.Itoa(j) + ".txt"
+		data.Make(filename)
+	}
+
+	// this copies data, so can't adjust later
 	var cache Cache
-	cache.Init(CACHE_SIZE, LRU)
+	cache.Init(CACHE_SIZE, LRU, data)
 
 	for i := 0; i < 2; i++ {
 		for j := 0; j < (CACHE_SIZE + 1); j++ {
-			filename := "../input/" + strconv.Itoa(j) + ".txt"
-			f, _ := os.Create(filename) // ignore error if already exists
-			f.Close()
+			filename := "fake_" + strconv.Itoa(j) + ".txt"
 			_, err := cache.Fetch(filename)
 			if err != nil {
 				t.Errorf("Could not open %s from cache", filename)
 				failed = true
 			}
 		}
-	}
-
-	// clear up created files 
-	for j := 0; j < (CACHE_SIZE + 1); j++ {
-		filename := "../input/" + strconv.Itoa(j) + ".txt"
-		os.Remove(filename)
 	}
 
 	hits, misses := cache.Report()
@@ -71,32 +48,31 @@ func TestBasicLRUFail(t *testing.T) {
 	}
 }
 
-// assumes CACHE_SIZE <= 100
 func TestBasicLRUSuccess(t *testing.T) {
 	fmt.Printf("TestBasicLRUSuccess ...\n")
 	failed := false
 	misses := 0
 	
+	data := datastore.MakeDataStore()
+
+	// add files to datastore
+	for j := 0; j < CACHE_SIZE; j++ {
+		filename := "fake_" + strconv.Itoa(j) + ".txt"
+		data.Make(filename)
+	}
+
 	var cache Cache
-	cache.Init(CACHE_SIZE, LRU)
+	cache.Init(CACHE_SIZE, LRU, data)
 
 	for i := 0; i < 2; i++ {
 		for j := 0; j < CACHE_SIZE; j++ {
-			filename := "../input/" + strconv.Itoa(j) + ".txt"
-			f, _ := os.Create(filename) // ignore error if already exists
-			f.Close()
+			filename := "fake_" + strconv.Itoa(j) + ".txt"
 			_, err := cache.Fetch(filename)
 			if err != nil {
 				t.Errorf("Could not open %s from cache", filename)
 				failed = true
 			}
 		}
-	}
-
-	// clear up created files 
-	for j := 0; j < CACHE_SIZE; j++ {
-		filename := "../input/" + strconv.Itoa(j) + ".txt"
-		os.Remove(filename)
 	}
 
 	hits, misses := cache.Report()
