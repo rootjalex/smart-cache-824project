ce278dea2c23c466039535871f9d9eb5edf1cd9b
Meia <meia.alsup@gmail.com>
2020-05-02 15:40:01 -0400

    fix formatting

--- src/task.go
+++ src/task.go
@@ -2,8 +2,8 @@ package cache
 
 import (
     "os"
-	"./client"
-	"./cache"
+    "./client"
+    "./cache"
     "./datastore"
 )
 
@@ -11,36 +11,36 @@ import (
 
 // no need for mutex since only task runs at a time
 type AbstractBaseTask struct {
-	clients    []*client.Client
-	datastore  *datastore.DataStore
-	caches     map[int]*cache.Cache
+    clients    []*client.Client
+    datastore  *datastore.DataStore
+    caches     map[int]*cache.Cache
     hash       *cache.Hash
 }
 
 // TODO: datastore instead of files
 func NewAbstractBaseTask(numClients int, numCaches int, replicationFactor int, cacheType cache.CacheType, cacheSize int, datastore *datastore.DataStore, ms int) *AbstractBaseTask {
-	// make clients
-	clients := make([]*client.Client, numClients)
-	for i := range clients {
-		// TODO: implement and call Client constructor
-		clients[i] = &client.Client{}
-		// TODO: set their workloads somehow
-	}
+    // make clients
+    clients := make([]*client.Client, numClients)
+    for i := range clients {
+        // TODO: implement and call Client constructor
+        clients[i] = &client.Client{}
+        // TODO: set their workloads somehow
+    }
 
-	// make cache master
-	caches, hash := cache.StartTask(clients, cacheType, cacheSize, numCaches, replicationFactor, datastore, ms)
+    // make cache master
+    caches, hash := cache.StartTask(clients, cacheType, cacheSize, numCaches, replicationFactor, datastore, ms)
 
-	// TODO: add chache size
+    // TODO: add chache size
 
-	// ends := cacheMaster.getCacheEnds()
-	// assign end e for each client c
+    // ends := cacheMaster.getCacheEnds()
+    // assign end e for each client c
 
-	return &AbstractBaseTask{
-		clients: clients,
-		datastore: datastore,
-		caches: caches,
+    return &AbstractBaseTask{
+        clients: clients,
+        datastore: datastore,
+        caches: caches,
         hash: hash,
-	}
+    }
 }
 
 func (w *AbstractBaseTask) Launch() {
@@ -50,13 +50,13 @@ func (w *AbstractBaseTask) Launch() {
 // ------------------------------ ML Task
 
 type MLTask struct {
-	t *AbstractBaseTask
+    t *AbstractBaseTask
 }
 
 func NewMLTask(clients []client.Client, files []*os.File) *MLTask {
-	ml := &MLTask{}
-	// ml.aw = NewAbstractBaseTask(clients, files)
-	return ml
+    ml := &MLTask{}
+    // ml.aw = NewAbstractBaseTask(clients, files)
+    return ml
 }
 
 // ML
