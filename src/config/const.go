c1149c70510bc47db1109332e4abbe987d2b36e1
Amir Farhat <amirfarhat@Amirs-MacBook-Pro.local>
2020-05-05 22:22:40 -0400

    Forgot to add const.go with the CLIENT_COMPUTATION_DELAY

--- src/config/const.go
+++ src/config/const.go
@@ -1,7 +1,7 @@
 package config
 
 import (
-   "time"
+	"time"
 )
 
 const CACHE_SIZE = 50
@@ -12,19 +12,21 @@ const PREFETCH_SIZE = 5
 const SEED = 1
 
 type CacheType int
+
 const (
-    LRU            CacheType = 0
-    MarkovPrefetch CacheType = 1
-    MarkovEviction CacheType = 2
-    MarkovBoth     CacheType = 3
+	LRU            CacheType = 0
+	MarkovPrefetch CacheType = 1
+	MarkovEviction CacheType = 2
+	MarkovBoth     CacheType = 3
 )
 
 type DataType string
+
 // DATA_DEFAULT must be initialized to an empty instance of the above default
 const DATA_DEFAULT = ""
 
 const DATA_FETCH_TIME = time.Duration(100) * time.Millisecond
 
+const CLIENT_COMPUTATION_TIME = time.Duration(1) * time.Millisecond
+
 // latency stuff?
-
-
